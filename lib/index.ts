6c5024b7eaa5b138a46db06ef07de12a05089283
pillar <ymblender@gmail.com>
2021-08-21 19:42:38 +0800

    fix: add auto append Element

--- lib/index.ts
+++ lib/index.ts
@@ -1,31 +1,53 @@
 // 拼接string
-export const templateFn = function (...args: any[]): string {
+export const templateFn = function (...args: any[]): {
+  out: string;
+  eles?: { [key: string]: HTMLElement };
+} {
   if (args.length > 1) {
     const [str, ...rest] = args;
     let out = "";
-
+    let eleNum = 0;
+    const eles: { [key: string]: HTMLElement } = {};
     str.forEach((v: string, i: number) => {
       out += v;
-      if (rest[i]) {
-        out += rest[i];
+      const ri = rest[i];
+      if (typeof ri === "object") {
+        eleNum += 1;
+        out += `<span va-html="v${eleNum}" style=all:unset></span>`;
+        eles[eleNum] = ri;
+      } else if (ri) {
+        out += ri;
       }
     });
 
-    return out;
+    return { out, eles };
   }
   if (typeof args[0] === "string") {
-    return args[0];
+    return { out: args[0] };
   }
 
   if (args[0]) {
-    return args[0].join("");
+    return { out: args[0].join("") };
   }
 
-  return "";
+  return { out: "" };
 };
 
 export function html(...args: any[]): HTMLElement {
   const div = document.createElement("template");
-  div.innerHTML = templateFn(...args);
-  return div.content.firstElementChild as any;
+  const { out, eles } = templateFn(...args);
+  console.log(out, eles);
+  div.innerHTML = out;
+  const ele = div.content.firstElementChild as HTMLElement;
+  if (eles) {
+    Object.keys(eles).forEach((k) => {
+      const sub = ele.querySelector(`[va-html="v${k}"]`);
+      console.log(k, sub);
+      if (sub) {
+        sub.replaceWith((eles as any)[k]);
+        // ele.replaceChild(sub, (eles as any)[k]);
+      }
+    });
+  }
+  return ele;
 }
